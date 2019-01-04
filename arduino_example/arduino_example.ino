be86ff8298daa6db3841c9237448d35c98535306
kocjanr <kocjanr@onid.oregonstate.edu>
2019-01-03 19:41:00 -0800

    LED turns on at count 500

--- arduino_example/arduino_example.ino
+++ arduino_example/arduino_example.ino
@@ -1,17 +1,27 @@
+// TOOLS -> BOARD: Arduino/Genuiono Uno
+// TOOLS -> PORT: COM3
+
+// LED +: GND
+// LED -: Pin 13
+
 int ledPin = 13;
 int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(ledPin,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   count++;
+  Serial.println(count);
   digitalWrite(ledPin,LOW);
-  if(count == 200){
-  count = 0;
-  digitalWrite(ledPin,HIGH);
-  }
+  
+  if(count == 500){
+    Serial.println("LED ON");
+    count = 0;
+    digitalWrite(ledPin,HIGH);
+  }  
 }
