9c1c5282a0b3523d81bdc1b7264686886db2dee5
kocjanr <kocjanr@onid.oregonstate.edu>
2019-01-03 19:15:47 -0800

    Code update

--- arduino_example/arduino_example.ino
+++ arduino_example/arduino_example.ino
@@ -1,9 +1,17 @@
+int ledPin = 13;
+int count = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(ledPin,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  count++;
+  digitalWrite(ledPin,LOW);
+  if(count == 200){
+  count = 0;
+  digitalWrite(ledPin,HIGH);
+  }
 }
